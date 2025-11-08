ff52eb7b2fcf5235699dae2a624e9eb62073c269
sina nojavan <sina.nojavan.c@gmail.com>
2025-11-08 22:56:49 +0330

    test for maze vector

---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+	int w = 3, h = 3;
+	int array[w][h] = {
+		{0,0,1,},
+		{0,1,0,},
+		{0,0,0,},
+
+	};
+	pair<int,int> ptr = {0,0};
+
+int main(){
+	
+	
+	
+	return 0;
+}
