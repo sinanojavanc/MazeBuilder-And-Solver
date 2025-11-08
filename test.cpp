1cea75908413ff76a6fe95020b08a6f42c9704f4
sina nojavan <sina.nojavan.c@gmail.com>
2025-11-08 23:07:30 +0330

    update cout

--- test.cpp
+++ test.cpp
@@ -11,13 +11,19 @@ const int aw = 3, ah = 3;
 	pair<int,int> ptr = {0,0};
 
 int main(){
-	
+	cout << " xxxxxxxxxxxxx" << endl;
 	for (int i = 0; i < aw; i++) {
 	  	for (int j = 0; j < ah; j++) {
-			cout << array[i][j] << " ";
+	  		cout << " x ";
+	  		if (array[i][j] == 1)
+	  			cout << "x";
+	  		else
+	  			cout << " ";
+			
 		}
+		cout << " x";
 	 	cout << endl;
 	}
-	
+	cout << " xxxxxxxxxxxxx";
 	return 0;
 }
